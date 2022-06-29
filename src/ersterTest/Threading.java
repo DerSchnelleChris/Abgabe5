4d20500a4a28a13eba7c93c020608b28c38cbfd7
tim <tim.schoppe@gmx.net>
2022-06-29 10:39:12 +0200

    Lock und Unlock

--- src/ersterTest/Threading.java
+++ src/ersterTest/Threading.java
@@ -2,13 +2,37 @@ package ersterTest;
 
 public class Threading extends Thread {
            static int cnt = 0;
+           static int b = 0;
 
            public static void main(String[] args)
            {
+               var lock = new java.util.concurrent.locks.ReentrantLock();
                 Thread t1 = new Threading();
                 Thread t2 = new Threading();
-                t1.start();
-                t2.start();
+                Thread t3 = new Thread(()-> {
+                    int localB = 0;
+                    for (int i = 0; i < 1000; ++i) {
+                        localB += 1;
+                    }
+                    lock.lock();
+                    b = b + localB;
+                    lock.unlock();
+                });
+                Thread t4 = new Thread(() -> {
+                    int localB = 0;
+                    for (int i=0; i<1000; ++i) {
+                        localB += 1;
+                    }
+                    lock.lock();
+                    b= b+localB;
+                    lock.unlock();
+                });
+                //t1.start();
+                //t2.start();
+                System.out.println("Lock und Unlock");
+                t3.start();
+                t4.start();
+                System.out.println(b);
               }
 
            public void run()
